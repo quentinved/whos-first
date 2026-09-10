b6887be398d5854b125180a69da6ddfb953cfee8
Quentin Vedrenne <qvedrenne@edamame.tech>
2026-09-11 01:46:37 +0200

    Initial Commit

---
+++ who is/who_isApp.swift
@@ -0,0 +1,17 @@
+//
+//  who_isApp.swift
+//  who is
+//
+//  Created by Quentin Vedrenne on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct who_isApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
